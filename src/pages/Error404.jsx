606bb68678da48e3eb1e0aae4a2fb122ae7d056c
LudoCzd <ludo.cazade@gmail.com>
2025-04-16 17:19:16 +0200

    Ajout main color, ajout police Montserrat, création et mise en page terminée pour page Error 404

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import "./Error404.scss";
+import { Link } from "react-router-dom";
 
 function Error404() {
-  return <div>Error404</div>;
+  return (
+    <div>
+      <div className="pageError">
+        <h1 className="titreError">404</h1>
+        <h2 className="messageError">
+          Oups! La page que vous demandez n'existe pas.
+        </h2>
+        <Link className="pageErrorToHome" to="/">
+          Retourner sur la page d'accueil
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Error404;
